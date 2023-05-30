20869d5f3d78c7b122cb8c5d4304141d3410163b
Мария <mahadrum@yandex.ru>
2023-05-30 14:23:34 +0300

    Добавьте файлы проекта.

---
+++ Angry/FormGame.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Angry
+{
+    public partial class FormGame : Form
+    {
+        Graphics gr;
+        public FormGame()
+        {
+            InitializeComponent();
+            gr = this.CreateGraphics();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void FormGame_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                GameHeroXY h = new GameHeroXY(gr, e.X, e.Y);
+                h.Show();
+            }
+            else
+            {
+                GameHeroXYPicture t = new GameHeroXYPicture(this, "images/angry.png", e.X, e.Y);
+                this.Controls.Add(t);
+            }
+        }
+    }
+}
---
+++ Angry/birds.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Angry
+{
+    class Birds
+    {
+    }
+
+    class GameHero
+    {
+        protected int x, y;
+        int r;
+        protected Graphics gr;
+
+        public GameHero(Graphics UserGR)
+        {
+            x = 100;
+            y = 100;
+            r = 10;
+            gr = UserGR;
+        }
+
+        public void Show()
+        {
+            gr.FillEllipse(Brushes.Red,x - r, y-r, 2*r, 2*r);
+        }
+    }
+
+    class GameHeroXY: GameHero
+    {
+        public GameHeroXY(Graphics UserGR, int UserX, int UserY):base(UserGR)
+        {
+            x = UserX;
+            y = UserY;
+        }
+    }
+
+    class GameHeroXYPicture : PictureBox
+    {
+        Form owner;
+        public GameHeroXYPicture(Form UserOwner, string FileName, int UserX, int UserY)
+        {
+            owner = UserOwner;
+            this.Left = UserX;
+            this.Top = UserY;
+            this.Load(FileName);
+            this.Width = 40;
+            this.Height = 40;
+            this.SizeMode = PictureBoxSizeMode.StretchImage;
+        }
+    }
+}
